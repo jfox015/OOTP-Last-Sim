0daa4e5791a21a1b3452a39f323acf6de7559040
Jeff Fox <jfox015@gmail.com>
2012-02-23 00:21:44 -0500

    Config Update

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
 $config['module_config'] = array(
 	'description'	=> 'Displays Information fromt he last OOTP Sim Period. Converted to CodeIgniter/HMVC by Jeff Fox.',
 	'authors'		=> 'Frank Esselink (fhommes) - Original Author, Jeff Fox (DesertFox07) - HMVC conversion',
-	'name'			=> 'Last SIm',
+	'name'			=> 'OOTP Last Sim',
     'version'       =>  '1.0'
 );
